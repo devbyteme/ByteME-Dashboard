c272967542485dac5d460afc7eb04ae6b1abe300
devbyteme <dev.bytemehospo@gmail.com>
2025-10-01 21:10:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: true,  
+    host: '0.0.0.0',     
+    port: 4173          
+  },
   server: {
     allowedHosts: true
   },
